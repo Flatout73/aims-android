74d3ea4c24773f620a31dc258ce1443cc4bced64
Flatout73 <fantom2259@gmail.com>
2016-12-11 21:28:21 +0300

    bugFixes

--- app/src/main/java/net/styleru/aims/AddTarget.java
+++ app/src/main/java/net/styleru/aims/AddTarget.java
@@ -1,20 +1,14 @@
 package net.styleru.aims;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 
 import net.styleru.aims.fragments.AddTarget1;
--- app/src/main/java/net/styleru/aims/fragments/addTarget1.java
+++ app/src/main/java/net/styleru/aims/fragments/addTarget1.java
@@ -1,7 +1,5 @@
 package net.styleru.aims.fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -16,9 +14,11 @@ import net.styleru.aims.R;
  * create an instance of this fragment.
  */
 public class AddTarget1 extends Fragment {
+
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM1 = "kek";
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
@@ -50,6 +50,7 @@ public class AddTarget1 extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -59,7 +60,9 @@ public class AddTarget1 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+        View view;
+        //inflater
         return inflater.inflate(R.layout.fragment_new_add_target_1, container, false);
     }
 }
